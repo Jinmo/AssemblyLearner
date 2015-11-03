d6b9e4c62c8f39d81f8c71896f8e28684904b48b
Jinmo <santoky001@naver.com>
2015-11-03 23:06:59 +0900

    템플릿에서 css 일부분 master.html로 통합, 에디터 테마 변경, 에디터 기능 틀 추가

--- asmlearner/static/js/problem.js
+++ asmlearner/static/js/problem.js
@@ -1,3 +1,12 @@
+/*
+ * problem.js for problem editor
+ *
+ * requires:
+ * var problem = {
+ *  id: <problem id>
+ * };
+ */
+
 jQuery(window).ready(function() {
     var $codeArea = $('#codeArea');
     var editor = CodeMirror.fromTextArea($codeArea[0], {
@@ -5,6 +14,33 @@ jQuery(window).ready(function() {
         mode: {
             name: 'gas',
             architecture: 'x86'
-        }
+        },
+        theme: 'dracula'
+    });
+
+    function compileCode(editor) {
+        var code = editor.getValue();
+        $.post('/problem/' + encodeURIComponent(problem.id) + '/run',
+               {
+                   code: code
+               },
+               function(response) {
+                   response = JSON.parse(response);
+                   if(response.status == 'solved') {
+                       solvedModal();
+                   } else if(response.status == 'fail') {
+                       fail();
+                   } else if(response.status == 'compileError') {
+                       compileError(response);
+                   } else {
+                       unknownError();
+                   }
+               }
+              );
+    }
+
+    editor.setOption('extraKeys', {
+        'Ctrl-Enter': compileCode,
+        'Command-Enter': compileCode
     });
 });
